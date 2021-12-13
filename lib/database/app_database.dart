9b77d51e670478a8fad5895f005c084573d3266f
Felipe Dias <adfelipealmeida@gmail.com>
2021-12-13 08:25:13 -0300

    refactory for async await

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,53 +2,48 @@ import 'package:new_byte_bank/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  // função do sqflite
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
+Future<Database> createDatabase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, 'bytebank.db');
 
-    // Função que vai abrir o banco de dados
-    return openDatabase(
-      path,
-      onCreate: (db, version) {
-        db.execute('CREATE TABLE contacts('
-            'id INTEGER PRIMARY KEY, '
-            'name TEXT, '
-            'account_number INTEGER)');
-      },
-      version: 1,
-      // onDowngrade: onDatabaseDowngradeDelete,
-    );
-  });
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute('CREATE TABLE contacts('
+          'id INTEGER PRIMARY KEY, '
+          'name TEXT, '
+          'account_number INTEGER)');
+    },
+    version: 1,
+    onDowngrade: onDatabaseDowngradeDelete,
+  );
 }
 
 // Função para salvar o contato
-Future<int> save(Contact contact) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contactMap = {};
+Future<int> save(Contact contact) async {
+  final Database db = await createDatabase();
+  final Map<String, dynamic> contactMap = {};
 
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNummber;
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNummber;
 
-    return db.insert('contacts', contactMap);
-  });
+  return db.insert('contacts', contactMap);
 }
 
-// Função para buscar todosos contatos
-Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = [];
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact = Contact(
-          map['id'],
-          map['name'],
-          map['account_number'],
-        );
-        contacts.add(contact);
-      }
+// Função para buscar todos os contatos
+Future<List<Contact>> findAll() async {
+  final Database db = await createDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+  final List<Contact> contacts = [];
 
-      return contacts;
-    });
-  });
+  for (Map<String, dynamic> row in result) {
+    final Contact contact = Contact(
+      row['id'],
+      row['name'],
+      row['account_number'],
+    );
+    contacts.add(contact);
+  }
+
+  return contacts;
 }
